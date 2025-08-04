5dc7ec0c14beefe4bbbf81e0359d213b46039010
phitly <phitly@outlook.com>
2025-08-04 14:10:43 -0400

    added filesr

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello friend');
+const myLibrary=[];
+function Book(title,author,pages){
+    this.title=title,
+    this.author=author,
+    this.pages=pages
+}
+function addBookToLib(title,author,pages){
+    const newbook=new Book(title,author,pages);
+    myLibrary.push(newbook)
+}
+
+addBookToLib('h','s',222)
+addBookToLib('hss','swwwww',322)
+
+function displayBook(){
+    for (const book of myLibrary){
+        console.log(book.title,book.author,book.pages);
+    }
+}
+
+displayBook();
