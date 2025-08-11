4df258670afca41fee5cd1c7325a904f83542eb2
phitly <phitly@outlook.com>
2025-08-11 13:59:20 -0400

    added and wrote codes

--- script.js
+++ script.js
@@ -1,21 +1,77 @@
 const myLibrary=[];
+//creating a book object with properties, basically defines the 
+//structure of each book object stored in myLib
 function Book(title,author,pages){
-    this.title=title,
-    this.author=author,
-    this.pages=pages
-}
-function addBookToLib(title,author,pages){
-    const newbook=new Book(title,author,pages);
-    myLibrary.push(newbook)
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.isRead=isRead;
+    //this generate unique ids 
+    this.id=crypto.randomUUID();
 }
 
-addBookToLib('h','s',222)
-addBookToLib('hss','swwwww',322)
+Book.prototype.toggleStatus=function(){
+    this.isRead=!this.isRead;
+}
 
+function addBookToLib(title,author,pages,isRead){
+    const newbook=new Book(title,author,pages,isRead);
+    myLibrary.push(newbook);
+    displayBook()
+}
+
+//function to display books
 function displayBook(){
-    for (const book of myLibrary){
-        console.log(book.title,book.author,book.pages);
-    }
-}
+    //choosing the div book-container elememnt, the var ref this dom for manip
+    const bookContainer=document.getElementById('book-container');
+    //clear existing content aka prev rendered book cards
+    bookContainer.innerHTML=''; 
+    //iterates over each Book object. book param rep curent book obj in iteration
+    myLibrary.forEach(book => {
+        //creating a book card var
+        //create new <div> ele for each book which acts as a "card"
+        //containg book's details
+        const bookCard=document.createElement('div');
+        // create a book-card class to style the cards appearances
+        bookCard.classList.add('book-card');
+        // adds a data id attribute to card linkin it to books id
+        bookCard.setAttribute('data-id',book.id);
 
-displayBook();
+        //populate the card
+        bookCard.innerHTML=`
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <p>${book.pages}</p>
+            <p>${book.isRead ? 'yes' : 'no'}</p>
+            <button class="toggle-read-btn" data-id="${book.id}">Toogle read</button>
+            <button class="remove-btn" data-id="${book.id}">Remove</button>
+            `;
+        bookContainer.appendChild(bookCard);       
+    });
+    // handles the button clicks make the "toggle read" and remove fucntional
+    //functin is called wheneber the lib changes (after removing, adding ect)
+    addButtonListeners();   
+};
+
+function addButtonListeners(){
+    document.querySelectorAll('.toggle-read-btn').forEach(button=>{
+        button.addEventListener('click',()=>{
+            const id=button.getAtrribute('data-id');
+            const book=myLibrary.find(b=> b.id===id);
+            if(book){
+                book.toggleStatus();
+                displayBook();
+            }
+        });
+    });
+    document.querySelectorAll('.remove-btn').forEach(button=>{
+        button.addEventListener('click',()=>{
+            const id=button.getAtrribute('data-id');
+            const index=myLibrary.findIndex(b=> b.id===id);
+            if(index!== -1){
+                myLibrary.splice(index,1);
+                displayBook();
+            }
+        });
+    });
+}
